7c26cb79bd06f1fae974bfc0a5d1a7dc2b515fd0
Joshua Alvarado <joshalva3@gmail.com>
2024-05-10 09:15:45 -0500

    math and rounding lecture end cleaned up UI with .toFixed more methods review when using numbers

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -279,4 +279,22 @@ console.log(Math.min(5, 18, `23`, 11, 2));
 console.log(Math.PI); // constant
 console.log(Math.PI * Number.parseFloat`10 px` ** 2); // radius
 console.log(Math.trunc(Math.random() * 6) + 1);
-const randomInt = (min, max) => Math.trunc(Math.random() * (max - min) + 1);
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1);
+console.log(randomInt(10, 20));
+
+// Rounding Intergers
+console.log(Math.trunc(23.3)); // removes decimal
+console.log(Math.round(23.9)); // rounds to nearest interger
+// .ceil >> rounds down
+// .floor >> rounds down
+// all these methods also do type coercion
+// floor & trunc both cut off decimal part however for negative numbers
+console.log(Math.trunc(-23.3)); // just cuts the decimal off
+console.log(Math.floor(-23.9)); // rounds up
+// .floor is probably a little bit better
+
+// Rounding decimals or floating point numbers
+console.log((2.7).toFixed(0)); // 3 >> returns a string
+console.log((2.7).toFixed(3)); // 2.700 will add 0's until third decimal point
+// .toFixed will always return a string and not a number
+// + turns string into number don't forget
